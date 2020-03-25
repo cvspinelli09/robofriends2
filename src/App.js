f1c3246f6b3a39e1f27e77867549e98075c4c62e
cvspinelli <cvspinelli09@gmail.com>
2020-03-25 21:04:32 +0000

    added files to the project created by create-react-app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CardList from "./CardList";
+import SearchBox from './SearchBox';
+import Scroll from './Scroll';
+import ErrorBoundry from './ErrorBoundry';
+// import { robots } from './robots';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+          robots: [],
+          searchfield: ""
+        };
+    }
+
+    componentDidMount() {
+      fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({ robots: users }));
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+    }
+
+    render() {
+        const filteredRobots = this.state.robots.filter(robot => {
+          return robot.name
+            .toLowerCase()
+            .includes(this.state.searchfield.toLowerCase());
+        });
+        return (
+          <div className="tc">
+            <h1>RobotFriends</h1>
+            <SearchBox searchChange={this.onSearchChange} />
+            <Scroll>
+              <ErrorBoundry>
+                <CardList robots={filteredRobots} />
+              </ErrorBoundry>
+            </Scroll>
+          </div>
+        );
+    }
+    
 }
 
 export default App;

5bccf8d7ffb3d876942d392bacc0745b82e558c6
cvspinelli <cvspinelli09@gmail.com>
2020-03-26 10:39:13 +0000

    Updates

---
+++ precache-manifest.ee8836984bb4b3e4e4b66a42ee8ea460.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75f6814436d543fbbc7253e507348be8",
+    "url": "/robofriends2/index.html"
+  },
+  {
+    "revision": "fa4d2a7bd0b78f1252f8",
+    "url": "/robofriends2/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "09caef10db1b1a114df2",
+    "url": "/robofriends2/static/css/main.839c46c2.chunk.css"
+  },
+  {
+    "revision": "fa4d2a7bd0b78f1252f8",
+    "url": "/robofriends2/static/js/2.f7d29807.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends2/static/js/2.f7d29807.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09caef10db1b1a114df2",
+    "url": "/robofriends2/static/js/main.fb95390b.chunk.js"
+  },
+  {
+    "revision": "a0091232566f9dc59055",
+    "url": "/robofriends2/static/js/runtime-main.bc52e398.js"
+  }
+]);

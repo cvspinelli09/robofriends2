db40c2787d38d68dcff1dcaab1a2a3a3094d4432
cvspinelli <cvspinelli09@gmail.com>
2020-03-28 10:05:20 +0000

    updating service worker for PWA deployment and apiCall const to be used in other component if necessary

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
-import { createLogger } from 'redux-logger';
+// import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 
 import './index.css';
@@ -11,7 +11,7 @@ import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 import { searchRobots, requestRobots } from './reducers';
 
-const logger = createLogger();
+// const logger = createLogger();
 
 const rootReducer = combineReducers({ searchRobots, requestRobots })
 const store = 
@@ -27,4 +27,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
